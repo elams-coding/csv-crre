f3502ac2ac187983e3ddd359912994c179913a2f
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-02 20:02:48 +0100

    Changement des classes
    Classes filles de GestionCSV

--- src/CreerCSV.java
+++ src/CreerCSV.java
@@ -1,21 +1,5 @@
 
-import java.util.Scanner;
-import java.util.logging.Logger;
 
-class CreerCSV {
+class CreerCSV extends GestionCSV {
 
-    private static Scanner sc = App.sc;
-    private static Logger logger = App.logger;
-
-    public static void creerCSV(String titre) {
-        App.titreChoix(titre);
-
-        String reponse;
-
-        System.out.print("Chemin du fichier CSV : ");
-        reponse = App.sc.nextLine();
-
-        if (!reponse.isBlank()) {
-        }
-    }
 }
--- src/LireCSV.java
+++ src/LireCSV.java
@@ -1,20 +1,5 @@
 
-import java.util.Scanner;
 
-class LireCSV {
+class LireCSV extends GestionCSV {
 
-    private static Scanner sc = App.sc;
-    private static Logger logger = App.logger;
-
-    public static void lireCSV(String titre) {
-        App.titreChoix(titre);
-
-        String reponse;
-
-        System.out.print("Chemin du fichier CSV : ");
-        reponse = App.sc.nextLine();
-
-        if (!reponse.isBlank()) {
-        }
-    }
 }
