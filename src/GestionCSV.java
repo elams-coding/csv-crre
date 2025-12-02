d022da7d91c3c2224ff9f6db1983b7eb93a68f35
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-02 21:53:53 +0100

    Gestion des validations de chemins

--- src/GestionCSV.java
+++ src/GestionCSV.java
@@ -1,4 +1,5 @@
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -9,31 +10,87 @@ class GestionCSV {
 
     protected static final Scanner sc = App.sc;
     protected static final Logger logger = App.logger;
+    private static Path path;
+    private static boolean existe = false;
+    private static boolean estFichier = false;
+    private static boolean estDossier = false;
+    private static boolean estFichierCSV = false;
 
     private static void askPath() {
         logger.info("Demande du chemin en cours");
 
-        String path;
+        String strPath;
+        // demander la saisie du chemin de l'utilsateur
         System.out.print("Chemin du fichier CSV : ");
-        path = sc.nextLine();
+        strPath = sc.nextLine().trim();
 
-        while (path.isBlank()) {
+        // saisir de nouveau le chemin tant que la saisie est vide
+        while (strPath.isBlank()) {
             logger.log(Level.WARNING, "Le chemin est vide");
 
             System.err.println("Erreur de saisie.");
             System.out.print("Recommencez: ");
-            path = sc.nextLine();
+            strPath = sc.nextLine().trim();
         }
 
-        isPathValidCSV(path);
+        // appel de la fonction pour vérifier que le chemin soit
+        // valide
+        isPathValid(strPath);
+    }
+
+    private static boolean isPathValid(String strPath) {
+        while (strPath.isBlank()) {
+            askPath();
+        }
+
+        path = Path.of(strPath);
+
+        existe = Files.exists(path);
+        estFichier = Files.isRegularFile(path);
+        estDossier = Files.isDirectory(path);
+
+        // ajouter dans les logs un message en fonction de
+        // l'existance ou non et le type d'élement dont il s'agit
+        if (existe && estFichier) {
+            logger.info("Le chemin spécifié \"{0}\" pointe sur un fichier (régulier)");
+        } else if (existe && estDossier) {
+            logger.info("Le chemin spécifié \"{0}\" pointe sur un dossier");
+        } else {
+            logger.log(Level.WARNING, "Le chemin spécifié \"{0}\" ne pointe ni sur un fichier (régulier), ni sur un dossier", path);
+        }
+
+        return existe && estFichier || estDossier;
     }
 
     private static boolean isPathValidCSV(String strPath) {
-        Path path = Path.of(strPath);
+        if (isPathValid(strPath)) {
+            // s'assurer que le chemin est bien celui mis en argument
+            path = Path.of(strPath);
 
-        boolean existe = Files.exists(path);
-        boolean estFichierCSV = Files.isRegularFile(path) && path.toString().toLowerCase().endsWith(".csv");
+            estFichierCSV = path.toString().toLowerCase().endsWith(".csv");
 
-        return existe && estFichierCSV;
+            // ajouter dans les logs un message en fonction du type de
+            // fichier
+            if (estFichierCSV) {
+                logger.info("Le fichier est un fichier CSV");
+            } else {
+                // obtenir le séparateur du Système
+                String separateur = File.separator;
+
+                // créer un tableau de noms présent dans le chemin donné
+                String[] noms = strPath.split(separateur);
+
+                // avoir l'index du dernier élement du tableau
+                int dernierElement = noms.length;
+                dernierElement--;
+
+                // conserver le dernier nom présent dans le tableau
+                String nomFichier = noms[dernierElement];
+
+                logger.log(Level.WARNING, "Le fichier \"{0}\" n'est pas un fichier CSV", nomFichier);
+            }
+        }
+
+        return estFichierCSV;
     }
 }
