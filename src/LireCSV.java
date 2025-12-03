54340715bc6326e3f56763740f7e6cd1f5bc5b2f
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-03 13:42:21 +0100

    Ajout de fonction main et verifierChemin

--- src/LireCSV.java
+++ src/LireCSV.java
@@ -1,5 +1,17 @@
 
-
 class LireCSV extends GestionCSV {
 
+    public static void main() {
+        verifierChemin();
+    }
+
+    private static void verifierChemin() {
+        // appel de la fonction pour demander le chemin du fichier
+        // CSV
+        String chemin = askPath();
+
+        while (!isPathValidCSV(chemin)) {
+            chemin = askPath();
+        }
+    }
 }
