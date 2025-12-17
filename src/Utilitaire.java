169a0745aadbce16420da2f32b8d21ff67a5ad5d
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-17 22:04:11 +0100

    Vérification de la provenance de l'appel de la fonction de choix
    Vérification qui permet d'exécuter les bons choix en fonction de la provenance des appels de fonctions et l'affichage correspondante en cas d'erreurs
    
    Ajout de la fonction pour quitter

--- src/Utilitaire.java
+++ src/Utilitaire.java
@@ -9,7 +9,7 @@ public class Utilitaire {
     /**
      * Demander le choix de l'utilisateur parmi les éléments du menu
      */
-    public static int choixMenu() {
+    public static int choixMenu(Object classObject) {
         logger.info("Saisie de l'utilisateur");
         String saisie;
 
@@ -25,7 +25,7 @@ public class Utilitaire {
             // caractères saisie
             saisie = App.SC.nextLine().trim();
 
-            // tester la reponse
+            // tester la reponse si elle n'est pas vide
             if (!saisie.isBlank()) {
                 // essayer de convertir la saisie en entier
                 try {
@@ -34,22 +34,46 @@ public class Utilitaire {
                     logger.log(Level.INFO, "Conversion de la saisie \"{0}\" en entier {1}",
                             new Object[]{saisie, reponse});
 
-                    if (reponse != App.CREER_CSV && reponse != App.LIRE_CSV && reponse != App.QUITTER) {
-                        logger.log(Level.WARNING, "Saisie incorrecte pour \"{0}\"", new Object[]{reponse});
-                        System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+                    if (classObject instanceof App) {
+                        if (reponse != App.CREER_CSV && reponse != App.LIRE_CSV && reponse != App.QUITTER) {
+                            logger.log(Level.WARNING, "Saisie incorrecte pour \"{0}\"", new Object[]{reponse});
+                            System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+                        }
+                    } else if (classObject instanceof LireCSV) {
+                        if (reponse != LireCSV.LECTURE_INTEGRALE && reponse != LireCSV.COLONNE_PAR_COLONNE) {
+                            logger.log(Level.WARNING, "Saisie incorrecte pour \"{0}\"", new Object[]{reponse});
+                            System.err.printf("Veuillez entrer %d ou %d.%n", new Object[]{LireCSV.LECTURE_INTEGRALE, LireCSV.COLONNE_PAR_COLONNE});
+                        }
+                    } else if (classObject instanceof CreerCSV) {
+                        // TODO : gérer le cas d'une saisie de choix dans la classe CreerCSV
                     }
                 } catch (NumberFormatException e) {
-                    logger.log(Level.SEVERE, "La saisie \"{0}\" n''est pas un entier", saisie);
-                    System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+                    logger.log(Level.SEVERE, "La saisie \"{0}\" n''est pas un entier", new Object[]{saisie});
+
+                    if (classObject instanceof App) {
+                        System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+                    } else if (classObject instanceof LireCSV) {
+                        System.err.printf("Veuillez entrer %d ou %d.%n", new Object[]{LireCSV.LECTURE_INTEGRALE, LireCSV.COLONNE_PAR_COLONNE});
+                    } else if (classObject instanceof CreerCSV) {
+                        // TODO : afficher le message d'erreur correspondant au cas du menu de la classe CreerCSV
+                    }
 
                     // garder la valeur impossible
                     reponse = -1;
                 }
             } else {
                 logger.log(Level.WARNING, "Saisie vide");
-                System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+
+                if (classObject instanceof App) {
+                    System.err.printf("Veuillez entrer %d ou %d ou %d.%n", new Object[]{App.CREER_CSV, App.LIRE_CSV, App.QUITTER});
+                } else if (classObject instanceof LireCSV) {
+                    System.err.printf("Veuillez entrer %d ou %d.%n", new Object[]{LireCSV.LECTURE_INTEGRALE, LireCSV.COLONNE_PAR_COLONNE});
+                } else if (classObject instanceof CreerCSV) {
+                    // TODO : afficher le message d'erreur correspondant au cas du menu de la classe CreerCSV
+                }
             }
-        } while (saisie.isBlank() || reponse != App.CREER_CSV && reponse != App.LIRE_CSV && reponse != App.QUITTER);
+        } while (saisie.isBlank() || reponse != App.CREER_CSV && reponse != App.LIRE_CSV && reponse != App.QUITTER
+                && reponse != LireCSV.LECTURE_INTEGRALE && reponse != LireCSV.COLONNE_PAR_COLONNE);
 
         logger.log(Level.INFO, "Option sélectionnée : \"{0}\" validée", new Object[]{reponse});
 
@@ -61,7 +85,7 @@ public class Utilitaire {
 
     public static void executerChoix(int choix, Object classObject) {
         // vérifier la provenance de l'appel de la méthode
-        if (classObject.getClass() == App.class) {
+        if (classObject instanceof App) {
             switch (choix) {
                 case App.CREER_CSV -> {
                     logger.info("Appel de la méthode pour créer un fichier CSV");
@@ -78,15 +102,28 @@ public class Utilitaire {
                 }
                 case App.QUITTER -> {
                     // appel de la fonction pour App.QUITTER
-                    App.quitter();
+                    quitter();
                 }
-                default ->
-                    throw new AssertionError();
             }
-        } else if (classObject.getClass() == LireCSV.class) {
-
-        } else if (classObject.getClass() == CreerCSV.class) {
-
+        } else if (classObject instanceof LireCSV) {
+            switch (choix) {
+                case LireCSV.LECTURE_INTEGRALE -> {
+                    LireCSV.lecture(LireCSV.chemin);
+                }
+            }
+        } else if (classObject instanceof CreerCSV) {
+            // TODO : gérer le cas d'une exécution de choix dans la classe CreerCSV
         }
+    }
+
+    /**
+     * Quitter et fermer l'application.
+     */
+    private static void quitter() {
+        logger.info("Fermeture de l'application");
+
+        System.out.println("Bye");
+
+        logger.info("Application fermé.");
     }
 }
