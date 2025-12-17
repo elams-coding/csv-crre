8e43c0c14886d647c51e176e7a66374e25a1bef0
El Amine Mahamoud <noah.mahamoud49@icloud.com>
2025-12-17 13:22:28 +0100

    Lecture du fichier CSV en console

--- src/LireCSV.java
+++ src/LireCSV.java
@@ -1,4 +1,8 @@
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 class LireCSV extends GestionCSV {
 
     private static final String TITRE = "Lecture de fichier CSV";
@@ -13,5 +17,29 @@ class LireCSV extends GestionCSV {
         do {
             chemin = askPath();
         } while (!isPathValidCSV(chemin));
+
+        lecture(chemin);
+    }
+
+    private static void lecture(String chemin) {
+        File fichierCSV = new File(chemin);
+
+        Path tempChemin = Path.of(chemin);
+
+        afficherMessage(tempChemin);
+
+        // lire le fichier
+        try (Scanner lecteur = new Scanner(fichierCSV)) {
+            while (lecteur.hasNextLine()) {
+                String ligne = lecteur.nextLine();
+                System.out.println(ligne);
+            }
+        } catch (Exception e) {
+            System.err.println("Une erreur s'est produite.");
+        }
+    }
+
+    private static void afficherMessage(Path chemin) {
+        System.out.printf("%nLecture du fichier : %s%n", chemin.getFileName());
     }
 }
